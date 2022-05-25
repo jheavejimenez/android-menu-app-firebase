07ea72895fd47434cd2bc6f556a00ed7320547cf
jheave <jimenezjheave123@gmail.com>
2022-05-25 23:52:05 +0800

    fix auth

--- app/src/main/java/com/example/menu_app_firebase/LoginActivity.java
+++ app/src/main/java/com/example/menu_app_firebase/LoginActivity.java
@@ -1,7 +1,73 @@
 package com.example.menu_app_firebase;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText inputEmail, InputPassword;
+    Button btnLogin;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        inputEmail = findViewById(R.id.inputEmail);
+        InputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> login());
+
+    }
+
+    private void login() {
+        String email = inputEmail.getText().toString().trim();
+        String password = InputPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            inputEmail.setError("Email is required");
+            inputEmail.requestFocus();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            inputEmail.setError("Please enter a valid email");
+            inputEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            InputPassword.setError("Password is required");
+            InputPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 8) {
+            InputPassword.setError("Minimum length of password should be 8");
+            InputPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, task -> {
+            if (task.isSuccessful()) {
+                finish();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            } else {
+                Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/example/menu_app_firebase/RegisterActivity.java
+++ app/src/main/java/com/example/menu_app_firebase/RegisterActivity.java
@@ -1,6 +1,11 @@
 package com.example.menu_app_firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +13,79 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
    FirebaseAuth mAuth;
+   EditText inputEmailRegister;
+   EditText inputPasswordRegister;
+   EditText inputConfirmPassword;
+   Button btnRegister;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+      getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
       setContentView(R.layout.activity_register);
       mAuth = FirebaseAuth.getInstance();
-      // create a btn for register user and set onclick listener
+
+      inputEmailRegister = findViewById(R.id.inputEmailRegister);
+      inputPasswordRegister = findViewById(R.id.inputPasswordRegister);
+      inputConfirmPassword = findViewById(R.id.inputConfirmPassword);
+      btnRegister = findViewById(R.id.btnRegister);
+
+      btnRegister.setOnClickListener(v -> registerUser());
+
+   }
+   private void registerUser() {
+      String email = inputEmailRegister.getText().toString().trim();
+      String password = inputPasswordRegister.getText().toString().trim();
+      String confirmPassword = inputConfirmPassword.getText().toString().trim();
+
+      if (email.isEmpty()) {
+         inputEmailRegister.setError("Email is required");
+         inputEmailRegister.requestFocus();
+         return;
+      }
+
+      if (!isEmailValid(email)) {
+         inputEmailRegister.setError("Email is not valid");
+         inputEmailRegister.requestFocus();
+         return;
+      }
+
+      if (password.isEmpty()) {
+         inputPasswordRegister.setError("Password is required");
+         inputPasswordRegister.requestFocus();
+         return;
+      }
+
+      if (!password.equals(confirmPassword)) {
+         inputConfirmPassword.setError("Password does not match");
+         inputConfirmPassword.requestFocus();
+      }
+
+      if (!isPasswordValid(password)) {
+         inputPasswordRegister.setError("Password is not valid");
+         inputPasswordRegister.requestFocus();
+         return;
+      }
+
+      mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+         if (task.isSuccessful()) {
+            finish();
+            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+         } else {
+            Toast.makeText(
+                    RegisterActivity.this,
+                    "Registration Failed: " + task.getException().getMessage(),
+                    Toast.LENGTH_SHORT
+            ).show();
+         }
+      });
+   }
+
+   private boolean isEmailValid(String email) {
+      return email.contains("@");
+   }
+
+   private boolean isPasswordValid(String password) {
+      return password.length() > 8;
    }
 }
