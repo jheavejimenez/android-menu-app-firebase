844f1949aa8296f1350d63bd4a2f34a3349ab436
jheave <jimenezjheave123@gmail.com>
2022-05-25 18:35:15 +0800

    add firebase in main activity

--- app/src/main/java/com/example/menu_app_firebase/MainActivity.java
+++ app/src/main/java/com/example/menu_app_firebase/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.menu_app_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
